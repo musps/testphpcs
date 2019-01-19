9180010abfc5107acaec34c010ad45e8e01856a8
Sercan Yildiz <ysercanz@gmail.com>
2019-01-19 22:01:06 +0100

    fix linter

--- app/index.php
+++ app/index.php
@@ -1,8 +1,8 @@
 <?php
 
 echo 'START';
-echo 'Nunc odio velit, volutpat auctor est suscipit, molestie pharetra ante. Cras consequat aliquam scelerisque. In hac habitasse platea dictumst.';
-echo 'Nunc odio velit, volutpat auctor est suscipit, molestie pharetra ante. Cras consequat aliquam scelerisque. In hac habitasse platea dictumst.';
-echo 'Nunc odio velit, volutpat auctor est suscipit, molestie pharetra ante. Cras consequat aliquam scelerisque. In hac habitasse platea dictumst.';
-echo 'Nunc odio velit, volutpat auctor est suscipit, molestie pharetra ante. Cras consequat aliquam scelerisque. In hac habitasse platea dictumst.';
+echo 'Cras consequat aliquam scelerisque. In hac habitasse platea dictumst.';
+echo 'Cras consequat aliquam scelerisque. In hac habitasse platea dictumst.';
+echo 'Cras consequat aliquam scelerisque. In hac habitasse platea dictumst.';
+echo 'Cras consequat aliquam scelerisque. In hac habitasse platea dictumst.';
 echo 'END';
